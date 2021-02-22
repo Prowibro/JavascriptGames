2e4fea18206b254a1bb1d10f1ac302d45c49560b
Clusternite <sterregovaert@gmail.com>
2021-02-22 21:00:06 +0100

    begin

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './styles/styles.scss';
+import Home from './pages/Home';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+}
