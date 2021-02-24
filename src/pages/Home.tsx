5b0d692e1f27eec7e966ee297f39a79e2d948d62
Clusternite <sterregovaert@gmail.com>
2021-02-24 22:02:06 +0100

    homecard btn

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,17 @@ const Home = () => {
   return (
     <>
       <h1>ElectronGames</h1>
-      <HomeCard className="" btnChildren="badinbbadgmdsfjiofds">
-        <Button></Button>hey
+      <HomeCard btnChildren="Play now Btn 2">
+        <h2>Name of the game</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
       </HomeCard>
     </>
   );
